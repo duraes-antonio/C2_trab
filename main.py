bf964cbbf064a6430e40a8cf10894610206edc8a
Joel Will <joel-willb@hotmail.com>
2019-05-25 14:26:39 -0300

    Stylizada no radar

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ import random
 from matplotlib.lines import Line2D
 
 qtd_pontos = 25
+angulo_busca = 45
 
-thetas = [math.radians(random.randint(0,360)) for i in range(qtd_pontos)]
-rs = [random.uniform(0.0, 3.0) for i in range(qtd_pontos)]
+min_alpha = 0.2
+max_alpha = 0.6
+
+thetas = np.array([math.radians(random.randint(0,360)) for i in range(qtd_pontos)])
+rs = np.array([random.uniform(0.0, 3.0) for i in range(qtd_pontos)])
 
 plt.ion()
 
@@ -18,15 +22,17 @@ ax = fig.add_subplot(projection="polar")
 circle_thetas = np.arange(0, 360).astype("float64")
 circle_thetas *= math.radians(1)
 circle_r = [3] * 360
-
 ax.fill(circle_thetas, circle_r, 'g')
 
-line1, = plt.polar([0, math.radians(30)],[0, 3])
-line2, = plt.polar([0, 0],[0, 3])
+alphas = np.array([(max_alpha - min_alpha) * i / (angulo_busca + 1) + min_alpha for i in range(angulo_busca + 2)])
+
+lines = []
+for i in range(angulo_busca + 1):
+    lines.append(plt.polar([0, math.radians(i)],[0, 3],color="#162e1a", alpha=alphas[i], linewidth=1)[0])
 
 pts: [Line2D] = []
 for x, y in zip(thetas, rs):
-    line, = plt.polar(x, y, 'ro')
+    line, = plt.polar(x, y, color='#00ff24',marker="o")
     pts.append(line)
     line.remove()
 
@@ -37,8 +43,9 @@ while True:
         max_degree = math.radians(30) + math.radians(theta)
         min_degree = math.radians(theta)
 
-        line1.set_xdata(max_degree)
-        line2.set_xdata(min_degree)
+        for i in range(angulo_busca + 1):
+            degree = math.radians(i) + math.radians(theta)
+            lines[i].set_xdata(degree)
 
         for pt in pts:
             pt_theta = pt.get_xdata()
@@ -52,4 +59,3 @@ while True:
 
         plt.draw()
         plt.pause(0.01)
-
