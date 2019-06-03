d238d35020827494022c53be3ee0fad67cc6d06b
duraes-antonio <garotoseis@gmail.com>
2019-06-03 07:38:29 -0300

    NEW: Relatório incomp + Imagens + GIF

--- src/main.py
+++ src/main.py
@@ -1,113 +1,141 @@
-import matplotlib.pyplot as plt
-import math
-import numpy as np
-import random
+from util import instalar_dependencias
 
-from matplotlib.lines import Line2D
-import matplotlib.colors as mcolors
+instalar_dependencias()
 
+from random import randint, uniform
+from typing import List
 
-def desenha_triangulos():
-    plt.polar(math.radians(0), 2.9, color="r", marker=">")
-    plt.polar(math.radians(90), 2.9, color="r", marker="^")
-    plt.polar(math.radians(180), 2.9, color="r", marker="<")
-    plt.polar(math.radians(270), 2.9, color="r", marker="v")
+from math import radians
+from matplotlib import animation, pyplot
+from matplotlib.patches import Wedge, Circle
+
+from Ponto import PontoPolar
 
 
-def desenha_background():
-    # Circulo verde
-    circle_thetas = np.arange(0, 360).astype("float64")
-    circle_thetas *= math.radians(1)
-    circle_r = [3] * 360
-    ax.fill(circle_thetas, circle_r, 'g')
+# Inicializa a estrutura do quadro onde os objetos serão desenhados
+global_fig = pyplot.figure()
+global_ax = global_fig.add_subplot(111, polar=True)
 
-    fill_color = "#162e1a"
-    zorder = 1
+# Número de graus que o ângulo de abertura se incrementará no sentido anti-horário
+global_increm_ang = 1
 
-    # Gera gradiente
-    z = np.empty((90, 1, 4), dtype=float)
-    rgb = mcolors.colorConverter.to_rgb(fill_color)
-    z[:, :, :3] = rgb
-    z[:, :, -1] = np.linspace(1, 0, 90)[:, None]
+# Valor que a opacidade do setor variará a cada movimento
+global_incremento_alpha = 0.01
 
-    # Desenha gradientes no radar
-    for quadrante in [0,1,2,3]:
-        xmin, xmax, ymin, ymax = math.radians(quadrante*90), math.radians((quadrante * 90)+90), 0, 3
-        im = ax.imshow(z, extent=[xmin, xmax, ymin, ymax], origin='upper', zorder=zorder)
+# Fator que define se a opcidade incrementará ou decrementará
+# Quando for 1, indica incremento; quando for -1, indica decremento
+global_multip_alpha = 1
+
+# Defina a largura e formato da linha da grade
+global_ax.grid(linewidth=0.75)
+global_pts_polar: List[PontoPolar] = []
 
 
-def update_animation():
-    """
-    Exibe os pontos dentro do ângulo busca e deixa os pontos fora do angulo de busca invisiveis
-    Faz as linhas andarem de acordo com o ângulo busca
-    """
+def desenhar_triangulos() -> None:
+	"""Marca no gráfico o 4 pontos triagulares nos extremos dos eixos e um central"""
 
-    # Faz o theta variar em 360 graus
-    for theta in range(0, 360, 3):
-        # Calcula o máximo e o mínimo grau do para o theta dentro do ângulo de busca
-        max_degree = math.radians(angulo_busca) + math.radians(theta)
-        min_degree = math.radians(theta)
-
-        # Anda com as linhas 3 graus nos sentido anti-horário
-        for i in range(angulo_busca + 1):
-            degree = math.radians(i) + math.radians(theta)
-            lines[i].set_xdata(degree)
-
-        # Passa por todos os pontos
-        for pt in pts:
-
-            # Pega o theta do ponto
-            pt_theta = pt.get_xdata()
-
-            # Se o ponto tiver entre o theta mínimo e o máximo, e não estiver na lista de pontos visiveis
-            if max_degree >= pt_theta >= min_degree and pt not in visible_pts:
-                visible_pts.append(pt)  # Adiciona o ponto na lista de pontos visiveis
-                ax.add_artist(pt)       # Deixa o ponto visivel no radar
-
-            # Senão, e o ponto estiver fora do intervalo e ainda for visivel
-            elif (pt_theta < min_degree or pt_theta > max_degree) and pt in visible_pts:
-                visible_pts.remove(pt)  # Tira o ponto da lista de visiveis
-                pt.remove()             # Deixa o ponto invisivel no radar
-
-        plt.pause(0.01)
+	pyplot.polar(radians(0), 0, color="r", marker="^")
+	pyplot.polar(radians(0), 2.9, color="r", marker=">")
+	pyplot.polar(radians(90), 2.9, color="r", marker="^")
+	pyplot.polar(radians(180), 2.9, color="r", marker="<")
+	pyplot.polar(radians(270), 2.9, color="r", marker="v")
 
 
-# Auto explicativo
-qtd_pontos = 7
-angulo_busca = 45
+def desenhar_background(ax) -> None:
+	"""Desenha o fundo da circunferência, projetando a sensação de gradiente"""
 
-# Gera os pontos
-thetas = np.array([math.radians(random.randint(0,360)) for i in range(qtd_pontos)])
-rs = np.array([random.uniform(0.0, 3.0) for i in range(qtd_pontos)])
+	decremento = 0.0104
+	raio = 0.52
+	cor = "#00{}0d"
+	cor_atual = 20
 
-# Gera figura e adiciona um novo axes
-fig = plt.figure()
-ax = fig.add_subplot(projection="polar")
+	for i in range(40):
+		circulo = Circle((0.5, 0.5), raio, transform=ax.transAxes)
+		circulo.set_facecolor(cor.format(hex(cor_atual).split('x')[-1].zfill(2)))
+		raio -= decremento
+		cor_atual += 5
+		ax.add_artist(circulo)
 
-desenha_background()
 
-min_alpha = 0.1
-max_alpha = 0.6
+def sortear_pontos(quantidade: int, abert_min: float) -> List[PontoPolar]:
+	"""Gera e retorna uma lista de pontos (contendo a tupla(theta, r))"""
+	alpha_interv = abert_min + 360
+	thetas = [randint(0, alpha_interv) for i in range(quantidade)]
+	rs = [uniform(0.0, 2.6) for i in range(quantidade)]
 
-# Gera lista de alphas para as linhas
-# Entre o min_alpha e max_alpha
-alphas = np.linspace(min_alpha, max_alpha, angulo_busca+1)
+	return [PontoPolar(theta, r) for theta, r in zip(thetas, rs)]
 
-# Gera as linhas
-lines = []
-for i in range(angulo_busca + 1):
-    lines.append(plt.polar([0, math.radians(i)],[0, 3], color="#002d04", alpha=alphas[i], linewidth=3)[0])
 
-# Plota os pontos, e os deixa invisiveis
-pts: [Line2D] = []
-for theta, r in zip(thetas, rs):
-    pt, = plt.polar(theta, r, color='#ff7e26', marker="o")  # Plota o ponto
-    pts.append(pt)                                          # Add na lista
-    pt.remove()                                             # Deixa o ponto invisivel
+def plotar_pontos(min_theta, max_theta, pontos: List[PontoPolar]) -> None:
+	# Para cada ponto na lista de entrada
+	for pt in pontos:
 
-# Lista de pontos visiveis
-visible_pts = []
+		# Se o ponto tiver entre o theta mínimo e o máximo
+		if max_theta >= pt.theta >= min_theta:
 
-# Loop Infinito
-while True:
-    update_animation()
+			if (pt.ponto_2d):
+				pt.atualizar_alpha()
+
+			else:
+				temp_pt_2d, = pyplot.polar(radians(pt.theta), pt.r,
+				                           color='#ff7e26', marker="o", alpha=0.1)
+				pt.ponto_2d = temp_pt_2d
+
+		else:
+			if pt.ponto_2d:
+				pt.ponto_2d.remove()
+				pt.ponto_2d = None
+
+
+def atualizar_setor(setor: Wedge, abertura_min: float) -> [Wedge]:
+	"""Atualiza os valores de theta e opacidade do setor, movimentando-o"""
+
+	global global_multip_alpha
+
+	# O valor de theta = valor atual + incremento (Se a soma não ultrapassar 360)
+	# Se a soma ultrapassar 360, o valor será o resto da divisão (soma / 360)
+	setor.set_theta1((setor.theta1 + global_increm_ang) % 360)
+	setor.set_theta2(setor.theta1 + abertura_min)
+
+	# Se a opacidade do setor for maior que 0.4 ou for menor que o valor mínimo:
+	# inverta de incremento para decremento ou vice-versa
+	if setor.get_alpha() >= 0.4 or setor.get_alpha() < global_incremento_alpha:
+		global_multip_alpha = (-1) * global_multip_alpha
+
+	setor.set_alpha(setor.get_alpha() + global_incremento_alpha * global_multip_alpha)
+
+	return [setor]
+
+
+def atualizar_grafico(self, setor: Wedge, abertura_min: float):
+
+	global global_pts_polar
+	qtd_atual = len(global_pts_polar)
+	global_pts_polar += sortear_pontos(7 - qtd_atual, abertura_min)
+
+	atualizar_setor(setor, abertura_min)
+	plotar_pontos(setor.theta1, setor.theta2, global_pts_polar)
+	global_pts_polar = [pt for pt in global_pts_polar if pt.ponto_2d]
+
+
+def main():
+	desenhar_triangulos()
+	desenhar_background(global_ax)
+	abert_min = 45
+
+	# Crie um setor de centro = (0.5, 0.5), r = 0.5, entre 0 e 45º
+	# E adicione-o ao quadro
+	setor: Wedge = Wedge((0.5, 0.5), 0.5, 0, abert_min, alpha=0.01, aa=True,
+	                     color="white", transform=global_ax.transAxes)
+	global_ax.add_artist(setor)
+
+	# A cada K intervalo: movimente o setor e renderize a figura
+	# com Q quadros por segundo
+	temp = animation.FuncAnimation(global_fig, func=atualizar_grafico, frames=353,
+	                               fargs=[setor, abert_min], interval=1)
+	pyplot.show()
+
+	return 0
+
+
+main()
